7fa930af19862467bc760eb158e430437994fcb4
ieroslav1 <yaraboom@rambler.ru>
2023-03-21 11:06:17 +0300

    1 commit

---
+++ src/main/java/pro/sky/calculator/Homework23CalculatorApplication.java
@@ -0,0 +1,14 @@
+package pro.sky.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework23CalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework23CalculatorApplication.class, args);
+        System.out.println("Hello!");
+    }
+
+}
